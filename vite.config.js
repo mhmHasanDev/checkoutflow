2aef05c6162ce2024c85bd6a97d389b513b9fbb3
mhmHasanDev <freelancermh31@gmail.com>
2026-03-17 00:07:59 +0600

    Fix modal to use stable filename

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -14,4 +14,16 @@ export default defineConfig({
         }),
         react(),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: (chunkInfo) => {
+                    if (chunkInfo.name === 'modal') {
+                        return 'assets/modal.js';
+                    }
+                    return 'assets/[name]-[hash].js';
+                },
+            },
+        },
+    },
 });
